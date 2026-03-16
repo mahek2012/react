515dd51e8e24988ccb3f3ec4b6a90de09abacdd7
mahek2012 <maheksavaliya2012@gmail.com>
2026-03-16 13:40:31 +0530

    keepnote

---
+++ keepnote/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Note from './components/Note'
+
+const App = () => {
+  return (
+    <>
+    {/* create a app that create keep notes */}
+    <Note/>
+    </>
+  )
+}
+
+export default App
