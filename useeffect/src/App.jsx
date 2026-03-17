b0aaa9794190a17c919981a5d18cf7e0459f4c15
mahek2012 <maheksavaliya2012@gmail.com>
2026-03-17 13:38:17 +0530

    useeffect

---
+++ useeffect/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ApiFetch from './components/ApiFetch'
+
+const App = () => {
+  return (
+    <>
+    <ApiFetch/>
+    </>
+  )
+}
+
+export default App
---
+++ useeffect/src/components/ApiFetch.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const ApiFetch = () => {
+
+    //Method 1
+    const Method1 = () => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then((rawdata) => {
+            console.log(rawdata);
+            return rawdata.json();
+        })
+        .then((data)=> {
+            console.log(data);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    };
+
+    //Method 2
+    //Method 3
+
+  return (
+    <>
+    <h1>API Fetching Methods</h1>
+    <div>
+        <h1>Method 1: Fetch - then - then</h1>
+        <button className='px-4 py-2 text-center rounded-md bg-black text-white focus:scale-95' onClick={Method1}>Get Data</button>
+    </div>
+    <h1>Method 2: Async Await</h1>
+    <h1>Method 3: Axios(Async Await)</h1>
+    </>
+  )
+}
+
+export default ApiFetch
